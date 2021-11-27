8cebb14b0d867440bcf0f3b074378649a900344f
Florian Mold <florian.mold@agnet.at>
2021-11-27 17:40:43 +0100

    workspace init

---
+++ lecture-exercise/Program.fs
@@ -0,0 +1,8 @@
+﻿module lecture_exercise.Program
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let sampleFunction1 x = x*x + 3
+
+let result1 = sampleFunction1 4573
